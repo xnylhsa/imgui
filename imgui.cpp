3ceed0335b993de0c6634327d81a96dc8bd277ec
Rokas Kupstys <rokups@zoho.com>
2020-08-24 11:59:28 +0300

    ImStrv: Support running under test framework.

--- imgui.cpp
+++ imgui.cpp
@@ -2950,7 +2950,8 @@ ImGuiID ImGuiWindow::GetID(ImStrv str)
     ImGui::KeepAliveID(id);
 #ifdef IMGUI_ENABLE_TEST_ENGINE
     ImGuiContext& g = *GImGui;
-    IMGUI_TEST_ENGINE_ID_INFO2(id, ImGuiDataType_String, str, str_end);
+    IM_IMSTR_ENSURE_HAS_END(str);
+    IMGUI_TEST_ENGINE_ID_INFO2(id, ImGuiDataType_String, str.Begin, str.End);
 #endif
     return id;
 }
@@ -2985,7 +2986,8 @@ ImGuiID ImGuiWindow::GetIDNoKeepAlive(ImStrv str)
     ImGuiID id = ImHashStr(str, seed);
 #ifdef IMGUI_ENABLE_TEST_ENGINE
     ImGuiContext& g = *GImGui;
-    IMGUI_TEST_ENGINE_ID_INFO2(id, ImGuiDataType_String, str, str_end);
+    IM_IMSTR_ENSURE_HAS_END(str);
+    IMGUI_TEST_ENGINE_ID_INFO2(id, ImGuiDataType_String, str.Begin, str.End);
 #endif
     return id;
 }
