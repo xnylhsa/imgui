c30fb3121d0425b805c6ca08e8903e370d679db8
XnylhsA <sdg1997@gmail.com>
2021-03-07 21:35:25 -0800

    readying Dear imgui for Singularity Engine use

---
+++ Inc/Common.h
@@ -0,0 +1,7 @@
+#ifndef SINGULARITY_DEAR_IMGUI_COMMON
+#define SINGULARITY_DEAR_IMGUI_COMMON
+
+#include "framework/Core/Inc/core.h"
+#include "framework/Math/Inc/EngineMath.h"
+
+#endif
---
+++ Inc/DearImgui.h
@@ -0,0 +1,12 @@
+#ifndef INCLUDED_DEAR_IMGUI_LIBRARY
+#define INCLUDED_DEAR_IMGUI_LIBRARY
+
+#include "imgui.h"
+#include "imgui_impl_android.h"
+#include "imgui_impl_dx12.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_vulkan.h"
+#include "imgui_impl_win32.h"
+#include "imgui_impl_osx.h"
+
+#endif
